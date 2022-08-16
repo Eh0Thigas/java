5cfda488c42a54481afdbdec39706181b9ca0585
Thiago <thiagolaurenti123@gmail.com>
2022-08-16 14:58:05 -0300

    teste

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem vindos ao git");
+        System.out.println("Bem vindos ao git ");
     }
 }
