e36d751e5b114da94e0bdfe786962f61928b52a6
Thiago <thiagolaurenti123@gmail.com>
2022-08-17 14:49:10 -0300

    da

--- Projeto/src/dia17_08/Operadores.java
+++ Projeto/src/dia17_08/Operadores.java
@@ -2,10 +2,6 @@ package dia17_08;
 public class Operadores {
     public static void main(String[]args){
 
-    
-    
-
-        //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         /* OPERADORES 
         ==
         != 
@@ -18,6 +14,7 @@ public class Operadores {
         relacionais 
         logicos
         ternarios
+        
 
         operador de atribuição 
         simbolo de igualdade ( = )
@@ -27,8 +24,13 @@ public class Operadores {
         char sexo = 'm';
         boolean doadorOrgao = false;
         Date datadenascimento = newDate();
-
-        */  
+        */ 
+        String nome = "Thiago";
+        int idade = 20;
+        double peso = 40.3;
+        char sexo = 'm';
+        boolean doadorOrgao = false;
+         
 
         
 
