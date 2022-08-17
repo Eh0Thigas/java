772fc9a3a9e67f829c82e16119b20e4a448e7a2f
Thiago <thiagolaurenti123@gmail.com>
2022-08-17 14:13:45 -0300

    s

--- Projeto/src/TipoDados.java
+++ Projeto/src/TipoDados.java
@@ -1,10 +1,12 @@
+import java.lang.reflect.Array;
+
 import javax.swing.JOptionPane;
 
 public class TipoDados {
     public static void main(String[]args){
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-        // TERÇA FEIRA; DIA 16/08/2022
+        /*  TERÇA FEIRA; DIA 16/08/2022
         byte idade = 20;
         short ano = 2022;
         int cep = 14095070;
@@ -39,6 +41,8 @@ public class TipoDados {
 
         //final double VALOR_DE_PI = 3.14;
         //VALOR_DE_PI = 3.15;
+
+        */
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
     // QUARTA FEIRA DIA 17/08/2022
@@ -78,6 +82,74 @@ public class TipoDados {
         System.out.println(st1);
         System.out.println(c3);
 
+        for(int i = 0; i <= 10; i++){
+            System.out.println("\n"+ st1 + " tem " + b1 + " reais na conta");
+            b1--;
+            System.out.println(st2 + " tem " + b2 + " reais na conta a mais que " + st1);
+            b2++;
+        }
+
+        /* Conversões (casting) 
+        Upcast(implicito) - promoção de um tipo 
+        DownCast - transforma uma variavel pra um tipo menor
+        */ 
+
+        byte by1;
+        short sh1 = 1000;
+        by1 = (byte) sh1; //downcast ..resultado -24; perda de informação
+        System.out.println("\n");
+        System.out.println(sh1);
+        System.out.println(by1);
+        
+        
+        long lo1;
+        int in1 = 10;
+        lo1 = in1; //  = 10
+        System.out.println("\n");
+        System.out.println(in1);
+        System.out.println(lo1);
+
+        int in2;
+        long lo2 = 1000000000000000000L;
+        in2 = (int) lo2; // = -148664554
+        System.out.println("\n");
+        System.out.println(lo2);
+        System.out.println(in2);
+
+        int in3;
+        long lo3 = 100001;
+        in3 = (int) lo3; // downcast sem perda pois entra no inteiro
+        System.out.println("\n");
+        System.out.println(lo3);
+        System.out.println(in3);
+
+        double do1;
+        float fl1 = 10.5f;
+        do1 = fl1; //upcast
+        System.out.println("\n");
+        System.out.println(fl1);
+        System.out.println(do1);
+
+        float fl2;
+        float fl3;
+        double do2 = 10000.58d; //downcast cabe dentro do float 
+        fl2 = (float) do2;
+        System.out.println("\n");
+        System.out.println(do2);
+        System.out.println(fl2);
+
+        double do3 = 10000.588888888888888888888888888888888888888888888888888d;
+        fl3 = (float) do3; // perda de informação
+        System.out.println("\n");
+        System.out.println(do3);
+        System.out.println(fl3);
+
+        int i4;
+        float f4 = 11.5687f;
+        i4 = (int) f4;
+        System.out.println("\n");
+        System.out.println(f4);
+        System.out.println(i4);
 
     }
 }
