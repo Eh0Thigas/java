509d7d99e60e53b944811015726cf002320513f3
Thiago <thiagolaurenti123@gmail.com>
2022-08-17 14:47:58 -0300

    mudando tudo

---
+++ Projeto/src/dia17_08/Operadores.java
@@ -0,0 +1,36 @@
+package dia17_08;
+public class Operadores {
+    public static void main(String[]args){
+
+    
+    
+
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        /* OPERADORES 
+        ==
+        != 
+        <
+        >
+        <=
+        >=
+        aritimeticos 
+        unarios
+        relacionais 
+        logicos
+        ternarios
+
+        operador de atribuição 
+        simbolo de igualdade ( = )
+        String nome = "Thiago"
+        int idade = 20;
+        double peso = 40.3;
+        char sexo = 'm';
+        boolean doadorOrgao = false;
+        Date datadenascimento = newDate();
+
+        */  
+
+        
+
+    }
+}
