f08ea10873400ebdeb60e9425e240ba62271cc11
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 14:31:16 -0300

    hh

--- Projeto/src/dia18_08/condicional.java
+++ Projeto/src/dia18_08/condicional.java
@@ -1,7 +1,9 @@
 package dia18_08;
-
+import java.util.Scanner;
 public class condicional {
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
         int numero1 = 1, numero2 = 2;
 
         //Ve se o numero 1 é igual ao numero 2
@@ -33,6 +35,63 @@ public class condicional {
 
         //equals = empresa1.equals(empresa2);
         //System.out.println(equals);
+
+        //-------------------------------------------------------------------------------
+
+        /*
+         CONDICIONAL E (and)
+            tem que ser as duas condições estejam verdadeiras 
+            |Condiçâo 1 |  Condição 2 | Resultado |
+            |    não    |     não     |   não     |
+            |    não    |     sim     |   não     |
+            |    sim    |     não     |   não     |
+            |    sim    |     sim     |   sim     |
+
+
+            CONDICIONAL OR (or)
+            tem que um das duas condições sejam verdadeiras 
+            |Condiçâo 1 |  Condição 2 | Resultado |
+            |    não    |     não     |   não     |
+            |    sim    |     não     |   sim     |
+            |    não    |     sim     |   sim     |
+            |    sim    |     sim     |   sim     |
+         */
+        //-------------------------------------------------------------------------------
+        //Exercicio 
+        String Banco = "Itau";
+        String Titular  = "jailson";
+        double SaldoInicial = 234.56;
+        double Retirada;
+        double SaldoFinal;
+
+        System.out.println("\nQual o nome do titular? ");
+        String TitularVef = s.nextLine();
+        System.out.println("\nQual o nome do Banco? ");
+        String BancoVef = s.nextLine();
+        
+
+        
+        if(TitularVef.equals(Titular) && BancoVef.equals(Banco)){
+            System.out.println("Banco " + Banco + "\n" + "Bem vindo " + Titular);
+        }else{
+            System.out.println("Acesso Negado");
+        }
+
+        System.out.println("\nQuanto você quer retirar? ");
+        Retirada = s.nextDouble();
+        
+        if(Retirada < SaldoInicial){
+            System.out.println("Retirada com Sucesso");
+            SaldoFinal = SaldoInicial - Retirada;
+            System.out.println("Saldo final de " + SaldoFinal );
+        }else{
+            System.out.println("Retirada Negada por falta de saldo");
+            System.out.println("Seu saldo é de " + SaldoInicial);
+        }
+
+
+
+
 
 
     }
