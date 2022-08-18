ca303b8ba7c919520fd89612c1523ed5428f3dac
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 15:18:08 -0300

    kmm

---
+++ Projeto/src/dia18_08/repeticao.java
@@ -0,0 +1,13 @@
+package dia18_08;
+
+public class repeticao {
+    public static void main (String[] args){
+        int idade = 15;
+
+        while(idade<=18){
+            System.out.println(idade);
+            //idade ++;
+            idade = idade + 1;
+        }
+    }
+}
