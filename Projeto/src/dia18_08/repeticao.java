68380db06c1581d3fc8bfc9879fa82ff7215c0f9
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 15:30:43 -0300

    ff

--- Projeto/src/dia18_08/repeticao.java
+++ Projeto/src/dia18_08/repeticao.java
@@ -3,13 +3,21 @@ package dia18_08;
 public class repeticao {
     public static void main (String[] args){
         int idade = 15;
+        
 
+        /*
+         * O WHILE normalmente usado para criar laços de repetições onde nao sabemos
+         * quantas repetições iremos fazer furante a execução do codigo.
+         * então: 
+         * while(condição de parada)  ou seja  enquando nao atender a condição de parada
+         * continuara a repetir o codigo dentro do while
+         */
         while(idade<=18){
             System.out.println(idade);
             //idade ++;
             idade = idade + 1;
         }
 
-        
+
     }
 }
