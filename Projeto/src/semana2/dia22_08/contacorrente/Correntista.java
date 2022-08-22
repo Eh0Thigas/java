ae5935d8018c14c558a7a6277ab35a2fd4534a70
Thiago <thiagolaurenti123@gmail.com>
2022-08-22 16:55:39 -0300

    ss

--- Projeto/src/semana2/dia22_08/contacorrente/Correntista.java
+++ Projeto/src/semana2/dia22_08/contacorrente/Correntista.java
@@ -10,16 +10,21 @@ public class Correntista {
         Scanner s = new Scanner(System.in);
 
         System.out.println(" --- Sistema De Conta Corrente --- ");
+
         System.out.println("\n");
         System.out.println("Seu nome por favor : ");
         String nome = s.nextLine();
+
         System.out.println("\n");
         System.out.println("Muito bem " + nome);
 
         conta.numeroConta =  numero.nextInt(1000);
         System.out.println("O numero da sua conta é: " + conta.numeroConta + " e agência: " + conta.agencia);
+        System.out.println("Saldo Disponivel: " + conta.saldo);
 
         
+      
+
 
     }
 }
