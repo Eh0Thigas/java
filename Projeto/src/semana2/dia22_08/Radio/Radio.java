62366d350de12a6f2927a1eb15370edce511cb5d
Thiago <thiagolaurenti123@gmail.com>
2022-08-22 16:25:06 -0300

    dd

---
+++ Projeto/src/semana2/dia22_08/Radio/Radio.java
@@ -0,0 +1,42 @@
+package semana2.dia22_08.Radio;
+
+public class Radio {
+    
+    int volume = 10;
+    boolean bluetooth = false;
+    boolean pause = false;
+    boolean ligada = false;
+
+    public void Ligar(){
+        ligada = true;
+    }
+
+    public void Desligar(){
+        ligada = false;
+    }
+
+    public void Pausar(){
+        pause = true;
+    }
+
+    public void Continuar(){
+        pause = false;
+    }
+
+    public void AumentarVolume(){
+        volume++;
+    }
+
+    public void DiminuirVolume(){
+        volume--;
+    }
+
+    public void Proucurar(){
+        bluetooth = true;
+    }
+
+    public void Normal(){
+        bluetooth = false;
+    }
+
+}
