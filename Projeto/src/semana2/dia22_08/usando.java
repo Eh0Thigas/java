1304ee30a9d9ad1e2e761c3130de26396ffef1d2
Thiago <thiagolaurenti123@gmail.com>
2022-08-22 16:23:53 -0300

    gg

--- Projeto/src/semana2/dia22_08/usando.java
+++ Projeto/src/semana2/dia22_08/usando.java
@@ -7,35 +7,68 @@ public class usando {
         Scanner s = new Scanner(System.in);
         Radio caixinha = new Radio();
 
-        String status , volume;
-        if(caixinha.ligada == true){
-            status = "Ligada";
-        }else{
-            status = "Desligada";
-        }
-        System.out.println("A caixinha de som está: "+ status);
-
-
+        System.out.println("A caixinha de som está: "+ caixinha.ligada);
+        System.out.println("Volume está em: " + caixinha.volume);
+        System.out.println("O Bluetooth "+ caixinha.bluetooth);
+// ----------------------------------------------------------------------------------------
+//---------------------------LIGAR---------------------------------------------------------
+        
         System.out.println("Quer ligar?");
         String ligar = s.nextLine();
 
-        if(ligar == "Sim"){
+        if(ligar.equals("sim")){
             caixinha.Ligar();
-            System.out.println("A caixinha de som está: "+ status);
-        }else if(ligar == "Não"){
+            System.out.println("A caixinha de som está: "+ caixinha.ligada);
+        }else if(ligar.equals("nao")){
             caixinha.Desligar();
-            System.out.println("A caixinha de som está: "+ status);
+            System.out.println("A caixinha de som está: "+ caixinha.ligada);
         }
+// ----------------------------------------------------------------------------------------
+//---------------------------VOLUME--------------------------------------------------------
 
-
+        System.out.println("\n");
         System.out.println("Aumentar ou Diminuir volume?");
-        volume = s.nextLine();
+        String volume = s.nextLine();
 
-        if(volume == "Aumentar"){
+        if(volume.equals("Aumentar")){
             caixinha.AumentarVolume();
-        }else if (volume == "Diminuir"){
+            System.out.println("Volume para " +caixinha.volume);
+        }else if (volume.equals("Diminuir")){
             caixinha.DiminuirVolume();
+            System.out.println("Volume para "+caixinha.volume);
         }
+
+// ----------------------------------------------------------------------------------------
+//---------------------------BLUETOOTH-----------------------------------------------------
+
+        System.out.println("\n");
+        System.out.println("Ligar Bluetooth?");
+        String blue = s.nextLine();
+
+
+        if(blue.equals("sim")){
+            caixinha.Proucurar();
+            System.out.println("Ligando bluetooth " + caixinha.bluetooth);
+        }else{
+            System.out.println("Desligando bluetooth " + caixinha.bluetooth);
+        }
+
+// ----------------------------------------------------------------------------------------
+//---------------------------PAUSE---------------------------------------------------------
+
+        System.out.println("\n");
+        System.out.println("Pause?");
+        String pause = s.nextLine();
+
+
+        if(pause.equals("sim")){
+            caixinha.Proucurar();
+            System.out.println("Pausando " + caixinha.pause);
+        }else{
+            caixinha.Normal();
+            System.out.println("Tocando " + caixinha.pause);
+        }
+
 
     }
 }
