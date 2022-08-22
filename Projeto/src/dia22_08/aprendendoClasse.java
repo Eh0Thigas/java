ded6d841c05853f8d05d10882ae7adf609270851
Thiago <thiagolaurenti123@gmail.com>
2022-08-22 14:13:47 -0300

    ff

---
+++ Projeto/src/dia22_08/aprendendoClasse.java
@@ -0,0 +1,18 @@
+/*
+         * Sempre que criamos um arquivo java, ira vir ja pé definido com 
+         * o nome da classe
+         *  e devera ser sempre em Maisculo a primeira letra
+         * ex : Classes.java    ira vir pre definido 
+         *          public class Classes(){
+         * 
+         *          }
+         * 
+         * e ser a classe for executavel 
+         * ira ter q ter o metodo unico 
+         *  public static void main(String[] args){
+         *  
+         *  }
+         * 
+         * é indicado que nos projetos tenha somente uma classe que tenha esse 
+         * metodo main
+         */
