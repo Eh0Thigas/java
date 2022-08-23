e4a8e5e1d5f5ee71c9f11957f0521f8ec8b5a578
Thiago <thiagolaurenti123@gmail.com>
2022-08-23 14:12:51 -0300

    dd

--- Projeto/src/semana2/dia23_08/Vendas/Salario.java
+++ Projeto/src/semana2/dia23_08/Vendas/Salario.java
@@ -1,9 +1,21 @@
 package semana2.dia23_08.Vendas;
 
+import java.util.Scanner;
+
 public class Salario {
     public static void main(String[] args) {
         
+        Scanner s = new Scanner(System.in); 
+        Vendas vendas = new Vendas();  
         
+        System.out.println(" -- Registrados de Vendas ---");
+        
+        System.out.println("Digite o nome do funcionario: ");
+        String nome = s.nextLine();
+        
+        System.out.println("Digite o salario fixo do " + nome + ": ");
+        vendas.salario = s.nextDouble();
 
+        
     }
 }
