aca58b5e98e13e3306c8f259dfac4d6ee8eeb434
Thiago <thiagolaurenti123@gmail.com>
2022-08-23 14:05:44 -0300

    çç

---
+++ Projeto/src/semana2/dia23_08/Vendas/Salario.java
@@ -0,0 +1,9 @@
+package semana2.dia23_08.Vendas;
+
+public class Salario {
+    public static void main(String[] args) {
+        
+        
+
+    }
+}
--- Projeto/src/semana2/dia23_08/Vendas/Vendas.java
+++ Projeto/src/semana2/dia23_08/Vendas/Vendas.java
@@ -1,5 +1,18 @@
 package semana2.dia23_08.Vendas;
 
 public class Vendas {
-    
+    String vendedor, produto;
+    double vlrProduto, salario , comissao;
+    int qntVendas;
+
+    public double CalComissao(int qntVendas, double vlrProduto){
+        comissao = (qntVendas * vlrProduto) * (15 / 100);
+        return comissao;
+    }
+
+    public double CalSalario(double comissao){
+        salario = salario + comissao;
+        return salario;
+    }
+
 }
