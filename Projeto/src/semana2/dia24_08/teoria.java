564caa6954338adb4a9213f6fa714dc74bc7f065
Thiago <thiagolaurenti123@gmail.com>
2022-08-24 13:30:53 -0300

    hh

---
+++ Projeto/src/semana2/dia24_08/teoria.java
@@ -0,0 +1,27 @@
+package semana2.dia24_08;
+
+public class teoria {
+    public static void main(String[] args) {
+        
+        /*
+         * 
+         * --------- VETORES E MATRIZES --------------
+         * 
+         * VETORES 
+         *  São sequencias de dados 
+         *  Estruturas de dados que contém itens 
+         *  do mesmo tipo
+         *  Uma vez criadas mantém o mesmo tamanho
+         *
+         *  vetor (array unidimensional)
+         *  matriz (array bidimensional)
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+    }
+}
