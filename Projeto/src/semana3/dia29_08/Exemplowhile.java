cf2a8419b13e41347263f32df69b3a0a0eb3dc7b
Thiago <thiagolaurenti123@gmail.com>
2022-08-29 13:23:24 -0300

    d

---
+++ Projeto/src/semana3/dia29_08/Exemplowhile.java
@@ -0,0 +1,30 @@
+package semana3.dia29_08;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Exemplowhile {
+    public static void main(String[] args) {
+        
+        double mesada = 50.0;
+
+            while(mesada>0){
+                double valorDoce = 8;//=  valorAleatorio();;
+                if(valorDoce > mesada){
+                    valorDoce = mesada;
+                }
+
+                System.out.println("Doce no valor de: " + valorDoce + " adicionado no carrinho");
+                mesada = mesada -  valorDoce;
+            }
+
+            System.out.println("Mesada: "+ mesada);
+            System.out.println("kleiton gastou a mesada inteira em doce");
+
+            /*metodo valor aleatorio
+            private stattic double valorAleatorio(){
+                return ThreadLocalRandom.current().nextDouble(2,0);
+            }
+            */
+
+    }
+}
